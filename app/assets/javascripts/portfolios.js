7fabd5e3ffba46ab8b8616264556e882375d80d0
CepCap <nikinoob@gmail.com>
2019-07-16 13:34:38 +0300

    Removing unused js file

--- app/assets/javascripts/portfolios.js
+++
@@ -1,8 +0,0 @@
-$(document).on('turbolinks:load', function() {
-  $('.delete-portfolio-link').on('click', function(e) {
-    var id = $(this)[0].id
-    console.log(id)
-    console.log($("div[portfolio-id=" + id + "]"))
-    $("div[portfolio-id=" + id + "]").remove();
-  })
-})
